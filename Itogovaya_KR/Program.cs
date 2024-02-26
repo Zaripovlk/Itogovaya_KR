8c2ebdf761d88189c2cd817467f5335ab8e8cdf5
Zaripovlk <zaripovlk@gmail.com>
2024-02-26 16:03:11 +0300

    add code Program.cs

--- Itogovaya_KR/Program.cs
+++ Itogovaya_KR/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку разделенную запятыми: ");
+        string input = Console.ReadLine()!;
+        string[] arr = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        string[] newArr = GetShortStrings(arr);
+
+        Console.WriteLine("Вывод нового массива:");
+        foreach (var item in newArr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string[] GetShortStrings(string[] arr)
+    {
+        int count = 0;
+        foreach (var item in arr)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArr = new string[count];
+        int index = 0;
+        foreach (var item in arr)
+        {
+            if (item.Length <= 3)
+            {
+                newArr[index++] = item;
+            }
+        }
+
+        return newArr;
+    }
+}
